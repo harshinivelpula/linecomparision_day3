3305dab4fd33178a5690dec3b8c3b1062a1e7aa4
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-30 11:53:31 +0530

    compare lines

--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@
     { 
        public static void Main(string[] args)
            {
+
             Console.WriteLine("welcome to line comparision computation problem");
-            
+            length Length = new length();
+            length.Length();
+            Console.ReadLine();
+            equal lc = new equal();
+            lc.length_line();
+            lc.function_check();
+            Console.ReadLine();
         }
     }
 }
